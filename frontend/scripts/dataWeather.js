b8c382bd8e6062d95d4abba6fb506b3c55e0ec36
davidrguez98 <ropeda98@gmail.com>
2025-04-19 11:40:50 +0200

    funcionamiento y diseño listo a falta de reestructuracion y diseño de tabla

--- frontend/scripts/dataWeather.js
+++ frontend/scripts/dataWeather.js
@@ -13,12 +13,12 @@ export async function getWeatherData(city) {
 
         for (let i of data.days.slice(0, 7)) {
             const day = {
-                date: i.datetime,
+                date: new Date(i.datetime).toLocaleDateString(),
                 conditions: i.conditions,
-                maxTemp: i.tempmax,
-                minTemp: i.tempmin,
-                precipitationProb: i.precipprob,
-                humidity: i.humidity,
+                maxTemp: ((i.tempmax - 32) * 5 / 9).toFixed(1)+"º",
+                minTemp: ((i.tempmin - 32) * 5 / 9).toFixed(1)+"º",
+                precipitationProb: i.precipprob+"%",
+                humidity: i.humidity+"%",
                 sunrise: i.sunrise,
                 sunset: i.sunset,
                 icon: i.icon
